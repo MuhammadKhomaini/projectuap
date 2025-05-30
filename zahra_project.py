e533821e687f84e77283bd72b52cdb4cbe33f32c
zahraayesha <zahraayesha0906@gmail.com>
2025-05-30 23:04:40 +0700

    Update zahra_project.py
    commit

--- zahra_project.py
+++ zahra_project.py
@@ -1,15 +1,1 @@
-Label(konten_menu, text="Selamat Datang di", font=("Rockwell", 30, "bold"), bg="white").pack()
-        Label(konten_menu, text="Project UAP Kelompok ", font=("Rockwell", 25, "bold"), bg="white").pack()
- 
-        Button(konten_menu, text="Perkenalan", font=("Poppins", 14), command=self.buka_perkenalan).pack(pady=20)
- 
-        self.frame_perkenalan = Frame(root, bg="white")
-       
-        konten_perkenalan = Frame(self.frame_perkenalan, bg="white")
-        konten_perkenalan.pack(expand=True)
- 
-        Label(konten_perkenalan, text="Perkenalan Kelompok", font=("Rockwell", 25, "bold"), bg="white").pack(pady=20)
- 
-        self.foto_perkenalan = []
-        isi_frame = Frame(konten_perkenalan, bg="white")
-        isi_frame.pack()
+
