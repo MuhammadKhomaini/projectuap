9b71be356b09dd9e193263dafaaa6841fb605b1e
MuhammadKhomaini <muhkhomaini22@gmail.com>
2025-05-30 21:54:28 +0700

    Update kel11.py

--- kel11.py
+++ kel11.py
@@ -1,1 +1,9 @@
+from tkinter import Tk, Frame, Button
+from PIL import Image, ImageTk
 
+class Menu:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Project UAP")
+        self.root.geometry("500x500")
+        self.root.resizable(True, True)
